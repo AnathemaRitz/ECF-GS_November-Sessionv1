3dabfd8db79a093b37fcd7d2ab4daba8d0afe40e
AnathemaRitz <149684298+AnathemaRitz@users.noreply.github.com>
2024-09-11 14:24:58 +0200

    Création de la mécanique de modification de mot de Passe

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Form\PasswordUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
 
 class AccountController extends AbstractController
 {
@@ -13,6 +19,28 @@ class AccountController extends AbstractController
     {
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
+        ]);
+    }
+    #[Route('/account/passwordUpdate', name: 'app_account_modify_pwd')]
+    public function password(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUserType::class, $user,[
+            'passwordHasher' => $passwordHasher,
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success',"Votre mot de passe a été mis à jour"
+            );
+
+        }
+
+        return $this->render('account/password.html.twig', [
+            'modifyPwd'=>$form->createView(),
         ]);
     }
 }
