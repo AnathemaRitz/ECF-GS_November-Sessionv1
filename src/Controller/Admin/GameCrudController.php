eb92a2fed265311ce02b5bcb0783a7c7b4421abb
AnathemaRitz <149684298+AnathemaRitz@users.noreply.github.com>
2024-09-15 03:03:55 +0200

    Création Entité "Game" et ajout de cette entité sur Easy Admin

---
+++ src/Controller/Admin/GameCrudController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Game;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+
+
+class GameCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Game::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Jeu')
+            ->setEntityLabelInPlural('Jeux');
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        $required = true;
+
+        if ($pageName=='edit') {
+            $required= false;
+
+        }
+
+        $currentYear = date("Y");
+        $releaseYearRange = range(1972, $currentYear);
+        $releaseYearRange = array_reverse($releaseYearRange);
+
+        return [
+
+            IdField::new('ID')->setLabel("Id ")->onlyOnIndex(),
+            AssociationField::new('genre', 'Genre'),
+            TextField::new('name')->setLabel('Titre')->setHelp("Titre du jeu"),
+            SlugField::new('slug')->setLabel('URL')->setTargetFieldName('name')->setHelp("URL générée automatiquement.")->hideOnIndex(),
+            TextEditorField::new('description')->setLabel('Description')->setHelp("Description du jeu"),
+            ChoiceField::new('pegi')->setLabel('PEGI')->setChoices([
+               '3'=> '3',
+                '7'=> '7',
+                '12'=> '12',
+                '16'=> '16',
+                '18'=> '18',
+            ]),
+
+            ChoiceField::new('releaseYear')
+                ->setLabel('Année de sortie')
+                ->setHelp('Année de sortie du jeu (Facultatif) ')
+                ->setChoices(array_combine($releaseYearRange, $releaseYearRange)),
+
+            TextField::new('publisher')->setLabel('Éditeur')->setHelp("Nom de l'éditeur du jeu (Facultatif)"),
+
+            ImageField::new('image')
+                ->setLabel('Image')
+                ->setHelp('Image du jeu')
+                ->setUploadedFileNamePattern('[year]-[month]-[day]-[contenthash].[extension]')
+                ->setBasePath("/uploads")
+                ->setUploadDir('/public/uploads')
+                ->setRequired($required),
+
+            MoneyField::new('price')->setLabel('Prix TTC')->setCurrency('EUR')->setHelp('Prix unitaire TTC du jeu.')->setStoredAsCents(false)
+
+
+            /*NumberField::new('stock')->setLabel('Stock')->setHelp('Quantité en stock'),
+            DateTimeField::new("createdAt")->setLabel('Date de création')->onlyOnIndex(),
+            DateTimeField::new("updatedAt")->setLabel('Date de modification')->onlyOnIndex(),*/
+
+        ];
+    }
+}
+
+
+
--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GenreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GenreRepository::class)]
@@ -18,6 +20,22 @@ class Genre
 
     #[ORM\Column(length: 255)]
     private ?string $slug = null;
+
+    /**
+     * @var Collection<int, Game>
+     */
+    #[ORM\OneToMany(targetEntity: Game::class, mappedBy: 'genre', orphanRemoval: true)]
+    private Collection $games;
+
+    public function __construct()
+    {
+        $this->games = new ArrayCollection();
+    }
+
+    public function __toString()
+    {
+       return $this->name;
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +62,36 @@ class Genre
     public function setSlug(string $slug): static
     {
         $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Game>
+     */
+    public function getGames(): Collection
+    {
+        return $this->games;
+    }
+
+    public function addGame(Game $game): static
+    {
+        if (!$this->games->contains($game)) {
+            $this->games->add($game);
+            $game->setGenre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGame(Game $game): static
+    {
+        if ($this->games->removeElement($game)) {
+            // set the owning side to null (unless already changed)
+            if ($game->getGenre() === $this) {
+                $game->setGenre(null);
+            }
+        }
 
         return $this;
     }
