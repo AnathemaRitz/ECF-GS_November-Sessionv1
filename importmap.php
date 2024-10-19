c5b0f3ca2ff705c802508074c7e33faceebf4d9d
AnathemaRitz <149684298+AnathemaRitz@users.noreply.github.com>
2024-10-19 19:44:13 +0200

    Clean base project and packages reinstall. HomeController.php creation.

--- importmap.php
+++ importmap.php
@@ -12,27 +12,8 @@
  * The "importmap:require" command can be used to add new entries to this file.
  */
 return [
-    /* 'app' => [
-         'path' => './assets/app.js',
-         'entrypoint' => true,
-     ],
-     '@hotwired/stimulus' => [
-         'version' => '3.2.2',
-     ],
-     '@symfony/stimulus-bundle' => [
-         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
-     ],
-     '@hotwired/turbo' => [
-         'version' => '7.3.0',
-     ],*/
-    'bootstrap' => [
-        'version' => '5.3.3',
-    ],
-    '@popperjs/core' => [
-        'version' => '2.11.8',
-    ],
-    'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.3',
-        'type' => 'css',
+    'app' => [
+        'path' => './assets/app.js',
+        'entrypoint' => true,
     ],
 ];
