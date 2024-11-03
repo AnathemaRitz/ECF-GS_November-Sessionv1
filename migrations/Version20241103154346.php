2d74eda9d85c74544ef99e57d940e7d4a3813dbf
AnathemaRitz <149684298+AnathemaRitz@users.noreply.github.com>
2024-11-03 17:02:43 +0100

    Customer Role : Creation of the class "Customer" inherited from User. Alteration of the security.yaml file and the database to implement this new type of user

--- migrations/Version20241103154346.php
+++ migrations/Version20241103154346.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241027112921 extends AbstractMigration
+final class Version20241103154346 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20241027112921 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE game ADD stock SMALLINT UNSIGNED NOT NULL');
+        $this->addSql('CREATE TABLE customer (address VARCHAR(255) NOT NULL, postal INT NOT NULL, city VARCHAR(255) NOT NULL, id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('ALTER TABLE customer ADD CONSTRAINT FK_81398E09BF396750 FOREIGN KEY (id) REFERENCES user (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE game DROP stock');
+        $this->addSql('ALTER TABLE customer DROP FOREIGN KEY FK_81398E09BF396750');
+        $this->addSql('DROP TABLE customer');
     }
 }
--- migrations/Version20241103155419.php
+++ migrations/Version20241103155419.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241019181157 extends AbstractMigration
+final class Version20241103155419 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241019181157 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD firstname VARCHAR(255) NOT NULL, ADD lastname VARCHAR(255) NOT NULL, ADD address VARCHAR(255) NOT NULL, ADD postal INT NOT NULL, ADD city VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE customer CHANGE postal postal SMALLINT UNSIGNED NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP firstname, DROP lastname, DROP address, DROP postal, DROP city');
+        $this->addSql('ALTER TABLE customer CHANGE postal postal INT NOT NULL');
     }
 }
