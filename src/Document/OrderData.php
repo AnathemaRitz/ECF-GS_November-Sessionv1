e189e455e569cdc8cf62b26bb499516a57bd493e
AnathemaRitz <149684298+AnathemaRitz@users.noreply.github.com>
2024-11-07 01:53:26 +0100

    Feature MONGODB : Configuration of a MONGODB Database and its bundles inside the project for the future Order Stats Dashboard

---
+++ src/Document/OrderData.php
@@ -0,0 +1,19 @@
+<?php
+/*
+namespace App\Document;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+#[MongoDB\Document]
+
+class OrderData
+{
+    #[MongoDB\Id]
+    protected string $id;
+
+    #[MongoDB\Field(type: 'string')]
+    protected string $name;
+
+    #[MongoDB\Field(type: 'float')]
+    protected float $price;
+}*/
+
