6df90c304e89a7fe2674d0c9d2a8526e16ab54de
AnathemaRitz <149684298+AnathemaRitz@users.noreply.github.com>
2024-11-17 08:29:46 +0100

    Feature : Creation of the SellerDashboardController that gets all the orders from the store the seller is attached to. Creation of a "order" view for the seller to change the state of the order

---
+++ src/Controller/SellerDashboardController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\OrderRepository;
+
+
+class SellerDashboardController extends AbstractController
+{
+
+    #[Route('/seller/dashboard', name: 'app_seller_dashboard')]
+    public function index(OrderRepository $orderRepository): Response
+    {
+        $user = $this->getUser();
+        $isSeller = $user->getRoles();
+        $isSeller = in_array('ROLE_SELLER', $isSeller);
+
+        if (!$isSeller) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $seller = $user;
+        $store= $seller->getStore();
+
+        $orders = $orderRepository->findBy([
+        'pickUpStore' => $store,
+         ]);
+
+
+        return $this->render('seller_dashboard/index.html.twig', [
+            'orders' => $orders,
+            'store' => $store,
+            'controller_name' => 'SellerDashboardController',
+        ]);
+    }
+    #[Route('/seller/order/{id_order}', name: 'app_seller_order', methods: ["GET"])]
+
+    public function getOrderView($id_order, OrderRepository $orderRepository, EntityManagerInterface $entityManager): Response
+    {
+
+        $order = $orderRepository->findOneBy([
+            'id' => $id_order
+        ]);
+
+        if (!$order) {
+            return $this->redirectToRoute('app_seller_dashboard');
+        }
+
+        return $this->render('seller_dashboard/order.html.twig', [
+            'order' => $order,
+        ]);
+    }
+
+    #[Route('/seller/order/{id_order}/state{state}', name: 'app_seller_order_update')]
+    public function setOrderState ($id_order, $state, OrderRepository $orderRepository, EntityManagerInterface $entityManager): Response
+    {
+       $order = $orderRepository->findOneBy([
+            'id' => $id_order
+        ]);
+
+        if (!$order) {
+            return $this->redirectToRoute('app_seller_dashboard');
+        }
+
+        $order->setState($state);
+        $entityManager->persist($order);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_seller_order', ['id_order' => $id_order]);
+    }
+}
