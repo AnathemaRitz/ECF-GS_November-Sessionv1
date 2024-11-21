b23a4103d056411e216b3aa715f4ddf621baa657
AnathemaRitz <149684298+AnathemaRitz@users.noreply.github.com>
2024-11-21 03:40:12 +0100

    Front : Refactoring of the base.twig.html

--- src/Class/Mail.php
+++ src/Class/Mail.php
@@ -36,8 +36,7 @@ class Mail {
                 'TemplateLanguage' => true,
                 'Subject' => $subject /*"My first Mailjet Email!"*/,
                     "Variables" => [
-                        "content" => $content /*"<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3>
-                <br />May the delivery force be with you!"*/
+                        "content" => $content
                     ]
             ]
         ]
--- src/Twig/AppExtensions.php
+++ src/Twig/AppExtensions.php
@@ -5,6 +5,7 @@ namespace App\Twig;
 use App\Class\Cart;
 use App\Repository\GameRepository;
 use App\Repository\GenreRepository;
+use App\Repository\StoreRepository;
 use Twig\Extension\AbstractExtension;
 use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
@@ -14,12 +15,14 @@ class AppExtensions extends AbstractExtension implements GlobalsInterface
     private $genreRepository;
     private $cart;
     private $gameRepository;
+    private $storeRepository;
 
-    public function __construct(GenreRepository $genreRepository, Cart $cart, GameRepository $gameRepository)
+    public function __construct(GenreRepository $genreRepository, Cart $cart, GameRepository $gameRepository, StoreRepository $storeRepository)
     {
         $this->genreRepository = $genreRepository;
         $this->cart = $cart;
         $this->gameRepository = $gameRepository;
+        $this->storeRepository = $storeRepository;
     }
     public function getFilters()
     {
@@ -38,7 +41,8 @@ class AppExtensions extends AbstractExtension implements GlobalsInterface
         return [
             'allGenres' => $this->genreRepository->findAll(),
             'allGames' => $this->gameRepository->findAll(),
-            'fullCartQuantity' => $this->cart->fullQuantity()
+            'fullCartQuantity' => $this->cart->fullQuantity(),
+            'allStores' => $this->storeRepository->findAll(),
         ];
     }
 
